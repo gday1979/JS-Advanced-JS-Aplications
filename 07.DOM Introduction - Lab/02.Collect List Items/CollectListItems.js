9ef4b7fd5a41d9d1443aa6c5c0015b2dd5ca3563
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-09-06 09:44:25 +0300

    update

--- 07.DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
+++ 07.DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+    let items = document.querySelectorAll('ul#items li');
+    let result = document.getElementById('result');
+
+    for (let item of items) {
+        result.value += item.textContent + '\n';
+    }
 }
