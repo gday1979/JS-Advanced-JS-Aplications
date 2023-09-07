b8dd1facdf1387ee814895ecea8053853378bdbf
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-09-07 09:40:10 +0300

    update

--- 08.DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 08.DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
+  const input = document.getElementById('input').value;
+  const sentences = input.split('.').filter(sentence => sentence.trim().length > 0);
+  let output = '';
+  if (sentences.length <= 3) {
+    output = `<p>${sentences.join('. ')}</p>`;
+  } else {
+    for (let i = 0; i < sentences.length; i += 3) {
+      const paragraph = sentences.slice(i, i + 3).join('. ');
+      output += `<p>${paragraph}</p>`;
+    }
+    if (sentences.length % 3 === 1) {
+      const lastParagraph = output.slice(output.lastIndexOf('<p>') + 3, output.lastIndexOf('</p>'));
+      output = output.slice(0, output.lastIndexOf('<p>')) + lastParagraph + '</p>';
+    }
+  }
+  document.getElementById('output').innerHTML = output;
 }
--- 08.DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ 08.DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -1,8 +1,3 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
-
-   function onClick () {
-      //   TODO:
-      
-   }
+   
 }
