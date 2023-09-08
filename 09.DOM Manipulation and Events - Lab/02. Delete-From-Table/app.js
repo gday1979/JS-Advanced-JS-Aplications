e548e09e4f9499c815a39bdcbf6c4094f8fb5d81
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-09-08 10:27:29 +0300

    update

--- 09.DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
+++ 09.DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,21 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let input = document.querySelector('input[name="email"]').value;
+    let result = document.getElementById('result');
+    let table = document.querySelector('tbody');
+    let rows = table.querySelectorAll('tr');
+    let deleted = false;
+
+    for (let row of rows) {
+        let email = row.querySelectorAll('td')[1].textContent;
+        if (email === input) {
+            row.remove();
+            deleted = true;
+        }
+    }
+
+    if (deleted) {
+        result.textContent = 'Deleted.';
+    } else {
+        result.textContent = 'Not found.';
+    }
 }
